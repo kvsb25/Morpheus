48a6ec5c24d08724d78353e50d9197659fe3cdad
Kovidh <kovidhvsbhati@gmail.com>
2026-06-02 19:09:57 +0530

    add GEMINI embedding function

--- agent/src/RAG/injestion/db.ts
+++ agent/src/RAG/injestion/db.ts
@@ -1,11 +1,16 @@
 import { ChromaClient, Metadata, QueryResult } from "chromadb";
+import { GoogleGeminiEmbeddingFunction } from "@chroma-core/google-gemini";
 const client = new ChromaClient({
-  host: "localhost",
+  host: "chroma",
   port: 8000
 });
 
-export const collection = await client.createCollection({
+export const collection = await client.getOrCreateCollection({
   name: "morpheus_collection",
+  embeddingFunction: new GoogleGeminiEmbeddingFunction({
+    apiKey: process.env.GEMINI_API_KEY,
+    modelName: "gemini-embedding-2",
+  })
 });
 
 export type ChromaDocument = QueryResult;
